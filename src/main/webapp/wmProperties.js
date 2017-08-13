eac7903d1ebeda738b9a642a42cbea4e63a65619
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-13 20:45:48 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_fhEOFPuxVz",
+  "homePage" : "Main",
+  "name" : "Auto_fhEOFPuxVz",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
